2562b2d4c33c0c435f91b7274fe0cc1f02259800
ItzhakS <ieshachnovitz@gmail.com>
2018-06-08 17:07:50 +0300

    Finish Students model

--- model/students_Model.php
+++ model/students_Model.php
@@ -4,7 +4,6 @@ class Students_Model extends Model {
 
     function __construct() {
         parent::__construct();
-//        parent::loadModel(__CLASS__);
     }
     
     public function Get($studentID){
@@ -32,7 +31,7 @@ class Students_Model extends Model {
         try {
             $sql = "INSERT INTO `theschool`.`students`(`ID`,`Name`,`phone`, `email`, `profile_image`) VALUES(:ID,:Name,:phone, :email, :profile_image);";
             if($_POST['ID'] == "" || $_POST['Name'] == "" || $_POST['phone'] == "" || $_POST['email'] == ""){
-                throw new Exception('Please Fill Out All Fields');
+                throw new Exception('Please Fill Out All Fields with a *');
             } else if($_POST['ID'] == "0" ){
                 throw new Exception('ID Cannot be zero');
             } else if(preg_match('~[0-9]+~', $_POST['Name'])){
@@ -63,18 +62,61 @@ class Students_Model extends Model {
             $stmt = $this->db->prepare($sql);
             $stmt->bindParam(':ID', $_POST['ID']);
             $stmt->bindParam(':Name', $_POST['Name']);
-            $stmt->bindParam(':EmpHireDate', $_POST['EmpHireDate']);
+            $stmt->bindParam(':phone', $_POST['phone']);
+            $stmt->bindParam(':email', $_POST['email']);
+            $stmt->bindParam(':profile_image', $_POST['profile_image']);
             $stmt->execute();
             if ($stmt->rowCount() == 0){
-                throw new Exception('No employee with that ID exists.');
-            } else if($_POST['EmpID'] == "" || $_POST['EmpName'] == "" || $_POST['EmpHireDate'] == ""){
-                throw new Exception('Please Fill Out All Fields');
+                throw new Exception('No Student affected.');
+            } else if($_POST['Name'] == "" || $_POST['phone'] == "" || $_POST['email'] == ""){
+                throw new Exception('Please Fill Out All Fields with a *');
             } else{
-            return "Successfully Updated employye number: ".$_POST['EmpID'];
+            return "Successfully Updated the students info";
             }
           } catch (Exception $ex) {
             return $ex->getMessage();
         }
     }
+    
+     public function Delete(){
+        try {
+            $sql = "DELETE FROM `theschool`.`students` WHERE ID = :ID;";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':ID', $_POST['ID']);
+            $stmt->execute();
+            if ($stmt->rowCount() == 0){
+                throw new Exception('No Student found.');
+            } else{
+            return "Student: ".$_POST['Name'].' Deleted!';
+            }
+          } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+    
+    public function GetAll(){
+        $sql = "SELECT * FROM `theschool`.`students`;";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $this->result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this;
+    }
+    
+     public function ToHTML(){
+         $data = $this->result;
+         $table= "<ul class='studentsList'>";
+         foreach ($data as $key => $value) {
+            $table .="<li class='studentListItem>";
+            $table .= "<div><a href='../Employees/CRUDALL/$value[ID]' target='_self'>";
+            $table .= "<div>$value[ID]</div>";
+            $table .= "<div>$value[Name]</div>";
+            $table .="<div>$value[phone]</div>";
+            $table .="<div>$value[email]</div>";
+            $table .="<div>$value[profile_image]</div>";
+            $table .= "</a></div></li>";
+         }
+         $table .= "</ul>";
+         return $table;
+     }
 
 }
