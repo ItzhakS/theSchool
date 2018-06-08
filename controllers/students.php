e0a24c1338f2cb3e4431c6b4fe9a97a947263015
ItzhakS <ieshachnovitz@gmail.com>
2018-06-08 18:24:35 +0300

    Add Crud and index to students controller

--- controllers/students.php
+++ controllers/students.php
@@ -5,11 +5,26 @@ class Students extends Controller {
     function __construct() {
         parent::__construct();
         parent::loadModel(__CLASS__);
-        
+    }
+
+    public function index(){
+        $this->_view->leftContent = "<h1> Courses</h1>";
+        $this->_view->rightContent = '<p>Lorem ipsum ;jhndn jnbouBAD  ;KJb k hid hk ia ibhaduofh kjhduofh thia haf to tbe th</p>';
+        $this->_view->render("leftContainer", "rightContainer");
+    }
+
+    public function crud(){
+        if($this->a){
+            $this->Get($this->a);
+        } else{
+        $Action = $_POST['ACTION'];
+        $Action = str_replace(' ', '', $Action);
+        $this->{$Action}();
+        }
     }
     
-    public function Get(){
-        $result = $this->_model->Get();
+    public function Get($studentID){
+        $result = $this->_model->Get($studentID);
         $this->_view->rightContent = $result;
         $this->_view->render("leftContainer", "rightContainer");
     }
