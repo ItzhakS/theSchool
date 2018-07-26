aae1d4e098ad15a4cd8e0e02363c6a006f9e0ced
ItzhakS <ieshachnovitz@gmail.com>
2018-07-26 19:41:10 +0300

    Block admin from seeing owner

--- views/administrators/rightAdminInfo.php
+++ views/administrators/rightAdminInfo.php
@@ -1,9 +1,13 @@
 <div class="adminInfo">
     <h3 class="adminName"><?php echo $this->name;?></h3>
     <img src="<?php echo $this->profile_image?>" class="profile_image">
-    <div class="adminPhone"><?php echo $this->role;?></div>
-    <div class="adminRole"><?php echo $this->phone;?></div>
+    <div class="adminPhone"><?php echo $this->phone;?></div>
+    <div class="adminRole"><?php echo $this->role;?></div>
     <div class="adminEmail"><?php echo $this->email;?></div>
+    <?php if(Session::get('role') == 'owner'): ?>
     <div class="editBtn"><a href="<?php echo Config::URL ?>administrators/EditAdmin/<?php echo $this->ID?>">Edit</a></div>
+    <?php elseif ($this->role == 'Administrator' && Session::get('role') == 'Administrator') :?>
+    <div class="editBtn"><a href="<?php echo Config::URL ?>administrators/EditAdmin/<?php echo $this->ID?>">Edit</a></div>
+    <?php endif?>
   </div>
 </div>
